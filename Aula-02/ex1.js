5ff6f25f04e7ceb01a48921fe990f3314bf4f257
GustavoFrancoFPP <gustavofranco080309@gmail.com>
2026-02-06 16:47:40 -0300

    .

--- Aula-02/ex1.js
+++ Aula-02/ex1.js
@@ -1,6 +1,8 @@
 function converterTemperatura() {
 let celsius = Number(document.getElementById("celsiusInput").value);
 
+let kelvin = celsius + 273.15;
+alert(`A temperatura em Kelvin é: ${kelvin}K`);
 let fahrenheit = celsius * 1.8 + 32;
 alert(`A temperatura em Fahrenheit é: ${fahrenheit}°F`);
 
